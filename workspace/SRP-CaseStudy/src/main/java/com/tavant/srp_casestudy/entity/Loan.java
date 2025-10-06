3b1d153fd5e556585be9cc5a6788295936ed8e30
PallaviPrasad <61450900+pallavi-jtrainer@users.noreply.github.com>
2025-10-06 12:53:32 +0530

    removed notification entity class. added loan entity class

---
+++ workspace/SRP-CaseStudy/src/main/java/com/tavant/srp_casestudy/entity/Loan.java
@@ -0,0 +1,42 @@
+package com.tavant.srp_casestudy.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "loans")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Loan {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private LocalDate loanDate;
+	private LocalDate dueDate;
+	private boolean returned;
+	
+	@ManyToOne(optional = false)
+	private Member member;
+	
+	@ManyToOne(optional = false)
+	private Book book;
+}
